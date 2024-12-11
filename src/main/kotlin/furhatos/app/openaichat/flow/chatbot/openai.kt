bd1503c7c08f51349af58c4959ae6771b64be104
Ian Steenstra <steenstra.ian@gmail.com>
2024-12-11 13:20:12 -0500

    fix

--- src/main/kotlin/furhatos/app/openaichat/flow/chatbot/openai.kt
+++ src/main/kotlin/furhatos/app/openaichat/flow/chatbot/openai.kt
@@ -247,6 +247,7 @@ class OpenAIChatbot() {
         if (conditionType == "intervention") {
             chatRequestBuilder.responseFormat(jsonSchema)
         }
+
         Furhat.dialogHistory.all.takeLast(10).forEach {
             when (it) {
                 is DialogHistory.ResponseItem -> {
@@ -254,7 +255,7 @@ class OpenAIChatbot() {
                 }
 
                 is DialogHistory.UtteranceItem -> {
-                    chatRequestBuilder.message(ChatMessage.AssistantMessage.of(it.toText()))
+                    chatRequestBuilder.message(ChatMessage.AssistantMessage.of(it.toUtterance().parts[0].toString()))
                 }
             }
         }
